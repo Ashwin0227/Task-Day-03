cdb00480ea9dc3cfb4abb5e8ebf2bede764dc79f
Ashwin kumar .S <117738392+Ashwin0227@users.noreply.github.com>
2023-03-04 17:21:59 +0530

    Add files via upload

---
+++ JSON-Resume/script.js
@@ -0,0 +1,57 @@
+let myResume={
+    "basics": {
+      "name": "ASHWIN KUMAR S",
+      "email": "ashwinsbahh271023@gamil.com",
+      "phone": 9566073594,
+      "degree": "B.SC Physics",
+      "D.O.B": 27.02.2001,
+      "Sex": Male,
+       "location": {
+        "address": "No.5 'A' Block AN Road, Chintadripet Police Quarters, ",
+        "postalCode": 600 002,
+        "city": "Chennai",
+        "state": "Tamilnadu",
+        "country": "India"
+      },
+      "profile": [
+        {
+          "github":"https://github.com/Ashwin0227"
+        }
+      ]
+    },
+    "Education": [
+      {
+        "institution": "DG Vaishnav College, Chennai ",
+        "department": "Physics",
+        "batch start year": 2018,
+        "batch end year": 2021,
+        "CGPA": 5.89,
+      }
+    ],
+    "Leraning Objective":[
+        "name":[
+            "I am a active learner looking for a platform to develop my skills in Full Stack Development and find a job where I fit perfectly to learn and grow. 
+        ]
+    ]
+    "Skills and Abilities": [
+      {
+        "name": "Javascript,Html,Css",
+        "level": "Beginner",
+        "Skills": [
+          "Possess knowledge in Computer and Laptop Services, Good at problem solving, Motivated individual, Has computer Skills"
+        ]
+      }
+    ],
+    "Languages": [
+      {
+        "Language": "Tamil,English",
+      }
+    ],
+    "interests": [
+      {
+        "name": "I have so much  interest in sports and fitness.I'm a fitness trainer and  love going to Gym and Talking about workouts  ",
+      }
+    ]
+  }
+  console.log(myResume);
+  
